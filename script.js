141afc3f0597a090a3ed88d616a140458723bd02
Madison Kerndt <madison.kerndt@gmail.com>
2016-07-05 20:31:57 -0600

    Updated Html & JavaScript

---
+++ script.js
@@ -0,0 +1,42 @@
+// random integers in the interval [1, 100]
+var numberValue = (ParseInt(Math.floor(Math.random()*100));
+var box = document.querySelector('.box');
+var guess = document.querySelector('.guess');
+var numberPlaceholder = document.querySelector('.number_placeholder');
+var numberHint = document.querySelector('.number_hint');
+var bestGuess = document.querySelector('.best_guess');
+var clear = document.querySelector('.clear');
+
+var clear = "";
+
+bestGuess.addEventListener('click', function () {
+
+  box.innerText = document.querySelector.value;
+});
+
+bestGuess.addEventListener('click', function () {
+  var bestGuess = a;
+  if a = cssValueInput.value;
+  box.style[cssProperty] = cssValue;
+});
+//input filed for guessing a number
+//clear input field
+//reset game
+//display most recent guess
+//error/ hint message
+
+function Hint (){
+  var numberPlaceholder = a
+  if (a = numberValue);
+    return "You WON! NICE WORK!"
+  else if (a > numberValue);
+    return "Your guess is too high! Try again."
+  else
+    return "Your guess is too low. Try again."
+});
+
+
+function (parseInt("string")
+  return = "";
+
+}
