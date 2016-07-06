88e1f7f560f48d8dce2c5f4ebeb6d06d1177abe6
Madison Kerndt <madison.kerndt@gmail.com>
2016-07-06 12:12:12 -0600

    Phase 1 draft

--- script.js
+++ script.js
@@ -1,42 +1,109 @@
-// random integers in the interval [1, 100]
-var numberValue = (ParseInt(Math.floor(Math.random()*100));
-var box = document.querySelector('.box');
+var numberValue = (Math.floor(Math.random()*100));
+//last number guessed
+var box = document.getElementById('box');
+//button press - submit guess
 var guess = document.querySelector('.guess');
-var numberPlaceholder = document.querySelector('.number_placeholder');
-var numberHint = document.querySelector('.number_hint');
-var bestGuess = document.querySelector('.best_guess');
+//input field
+var guessInput = document.querySelector('input');
+//clear button to clear field
 var clear = document.querySelector('.clear');
+//button to reset game
+var reset = document.querySelector('.reset');
 
-var clear = "";
+var recentGuess = document.getElementById('recent-guess');
+var displayHint = document.getElementById('hint');
 
-bestGuess.addEventListener('click', function () {
+document.addEventListener("DOMContentLoaded", function() {
 
-  box.innerText = document.querySelector.value;
+
+  // recentGuess.innerHTML("WOW")
+
+  // input field to guess number
+  //button to submit a guess
+  //displays most recent guess
+  //element that displays a message to user
+
+  // numberValue (guessInput, reset);
+  randomNumberGenerator(numberValue, reset);
+  grabAndDisplayGuess(guess, guessInput, displayHint);
+  clearGuess(clear, guessInput);
+
 });
 
-bestGuess.addEventListener('click', function () {
-  var bestGuess = a;
-  if a = cssValueInput.value;
-  box.style[cssProperty] = cssValue;
-});
-//input filed for guessing a number
-//clear input field
-//reset game
-//display most recent guess
-//error/ hint message
-
-function Hint (){
-  var numberPlaceholder = a
-  if (a = numberValue);
-    return "You WON! NICE WORK!"
-  else if (a > numberValue);
-    return "Your guess is too high! Try again."
-  else
-    return "Your guess is too low. Try again."
+var randomNumberGenerator = function (numberValue, reset) {
+  reset.addEventListener('click', function() {
+  randomNumberGenerator = (numberValue, reset);
 });
 
 
-function (parseInt("string")
-  return = "";
-
+var clearGuess = function(clear, guessInput) {
+  clear.addEventListener('click', function() {
+    guessInput.value = "";
+  });
 }
+
+var grabAndDisplayGuess = function(guess, guessInput,displayHint) {
+  guess.addEventListener('click',function(){
+    var guess = guessInput.value;
+    checkValue(guess,displayHint);
+    box.innerHTML = "<h1 id='latest-guess'>" + guess + "</h1>";
+});
+}
+
+var checkValue = function (guess, displayHint) {
+    if (guess===numberValue) {
+      displayHint.innerHTML="You Won!! Nice Work!";
+    } if (guess>numberValue) {
+      displayHint.innerHTML="Your guess is too high! Try again.";
+    } if (guess<numberValue) {
+      displayHint.innerHTML="Your guess is too low! Please try again!";
+    }
+}
+
+// console.log (randomNumberGenerator)
+
+  // function giveHint (){
+  //   var b = numberPlaceholder
+  //   var a = (ParseInt(Math.floor(Math.random()*100)));
+  //   if (b = a);
+  //     return "You WON! NICE WORK!"
+  //   else if (b > a);
+  //     return "Your guess is too high! Try again."
+  //   else
+  //     return "Your guess is too low. Try again."
+  // });
+//Random number generator
+
+//
+//
+//
+// //button to clearing the input field
+//
+// //button to resets game
+//
+//
+//
+// button.addEventListener ('click', function () {
+//   function Reset = (ParseInt(Math.floor(Math.random()*100)))
+// }
+//
+// button.addEventListener ('click', function (){
+//   bestGuess = "";
+// }
+//
+//
+// function giveHint (){
+//   var b = numberPlaceholder
+//   var a = (ParseInt(Math.floor(Math.random()*100)));
+//   if (b = a);
+//     return "You WON! NICE WORK!"
+//   else if (b > a);
+//     return "Your guess is too high! Try again."
+//   else
+//     return "Your guess is too low. Try again."
+// });
+//
+// bestGuess.addEventListener('click', function)
+// function clearValue(){
+//   return = "";
+// }
