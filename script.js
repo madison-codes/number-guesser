d7d76cdf2bc868c3ab2aab0a34bbf4666972eb76
MaryJane Valade <mjvalade@gmail.com>
2016-07-06 22:20:23 -0600

    Improve naming, add Clear button

--- script.js
+++ script.js
@@ -1,48 +1,21 @@
-
-// var newRandNum = randNum();
 var randNum = numberRandom();
-//last number guessed
+console.log('randNum on line 4: ', randNum);
 var box = document.getElementById('box');
-//button press - submit guess
-var guess = document.querySelector('.guess');
-//input field
-var guessInput = document.getElementById('guess-input');
-//button to reset game
-var reset = document.querySelector('.reset');
-
 var recentGuess = document.getElementById('recent-guess');
 var displayHint = document.getElementById('hint');
+var guessInputField = document.getElementById('guess-input-field');
+var guessButton = document.querySelector('.guess-button');
+var clearFieldButton = document.querySelector('.clear-field-button')
+var resetButton = document.querySelector('.reset-button');
 
-function numberRandom() {
-  var tmp = Math.floor(Math.random()*100);
-  // console.log(tmp);
-  return tmp;
-}
-
-function disableButton() {
-  guess.disabled = (guessInput.value === '');
-}
-//  “min” and a “max” attributes
-// display an error message if the guess is not a number (e.g. parseInt() returns NaN).
-// The application should display an error if the guess is outside of the range of possible answers.
-// The clear button should be disabled if there is nothing to clear.
-// The reset button should be disabled if there is nothing to reset.
-guessInput.addEventListener('keyup', disableButton);
+// rewrite
+guessInputField.addEventListener('keyup', disableButton);
 disableButton();
 
-reset.addEventListener('click', function(){
-  randNum =  numberRandom();
-  guessInput.value = "";
-  disableButton();
-  box.innerHTML = "<h1 id='latest-guess'>" + " " + "</h1>";
-  displayHint.innerHTML = "";
-  reset.disabled = true;
-});
-
-guess.addEventListener('click', function() {
+guessButton.addEventListener('click', function() {
   var max = 100;
   var min = 0;
-  var guess = parseInt(guessInput.value);
+  var guess = parseInt(guessInputField.value);
   if (!Number.isNaN(guess)) {
     if (guess < min) {
       displayHint.innerHTML="Your guess is too small for the range."
@@ -62,11 +35,40 @@ guess.addEventListener('click', function() {
   } else {
     displayHint.innerHTML="This is not a number."
   }
-  box.innerHTML = "<h1 id='latest-guess'>" + guessInput.value + "</h1>";
-  guessInput.value = "";
+  box.innerHTML = "<h1 id='latest-guess'>" + guessInputField.value + "</h1>";
+  clearInputFieldValue();
 });
 
-reset.disabled = true;
+clearFieldButton.addEventListener('click', clearInputFieldValue);
+
+resetButton.addEventListener('click', function() {
+  randNum = numberRandom();
+  console.log('randNum on line 41: ', randNum);
+  clearInputFieldValue();
+  disableButton();
+  box.innerHTML = "<h1 id='latest-guess'>" + " " + "</h1>";
+  displayHint.innerHTML = "";
+  resetButton.disabled = true;
+});
+
+function clearInputFieldValue() {
+  guessInputField.value = "";
+}
+
+function numberRandom() {
+  return Math.floor(Math.random()*100);
+}
+
+// disables the guessButton
+function disableButton() {
+  guessButton.disabled = (guessInputField.value === '');
+}
+
+
+
+
+
+resetButton.disabled = true;
 // var grabAndDisplayGuess = function(guess, guessInput,displayHint) {
 //   guess.addEventListener('click',function() {
 //     var guess = guessInput.value;
@@ -84,3 +86,16 @@ reset.disabled = true;
 //     displayHint.innerHTML="Your guess is too low! Please try again!";
 //   }
 // };
+
+
+
+// function clearInputField(selector, inputField) {
+//   selector.disabled = (inputField.value === '');
+// }
+// clearInputField(guess, guessInput);
+
+//  “min” and a “max” attributes
+// display an error message if the guess is not a number (e.g. parseInt() returns NaN).
+// The application should display an error if the guess is outside of the range of possible answers.
+// The clear button should be disabled if there is nothing to clear.
+// The reset button should be disabled if there is nothing to reset.
