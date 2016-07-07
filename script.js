b9097f4dce822427b20a5ae378421dfc84b45c00
MaryJane Valade <mjvalade@gmail.com>
2016-07-06 23:25:13 -0600

    reset-button-works

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var guessButton = document.querySelector('.guess-button');
 var clearFieldButton = document.querySelector('.clear-field-button')
 var resetButton = document.querySelector('.reset-button');
 
-// rewrite
 guessInputField.addEventListener('keyup', disableButton);
+// in startGame
 disableButton();
 
 guessButton.addEventListener('click', function() {
@@ -25,7 +25,7 @@ guessButton.addEventListener('click', function() {
       box.innerHTML = "<h1 id='latest-guess'>" + guess + "</h1>";
       if (guess === randNum) {
         displayHint.innerHTML="You Won!! Nice Work!";
-        reset.disabled = false;
+        resetButton.disabled = false;
       } else if (guess > randNum) {
         displayHint.innerHTML="Your guess is too high! Try again.";
       } else {
@@ -42,19 +42,20 @@ guessButton.addEventListener('click', function() {
 clearFieldButton.addEventListener('click', clearInputFieldValue);
 
 resetButton.addEventListener('click', function() {
+  // restart game (need function)
+  clearInputFieldValue();
   randNum = numberRandom();
   console.log('randNum on line 41: ', randNum);
-  clearInputFieldValue();
-  disableButton();
   box.innerHTML = "<h1 id='latest-guess'>" + " " + "</h1>";
-  displayHint.innerHTML = "";
   resetButton.disabled = true;
+  disableButton();
+  displayHint.innerHTML = "";
 });
 
 function clearInputFieldValue() {
   guessInputField.value = "";
 }
-
+// generates random number
 function numberRandom() {
   return Math.floor(Math.random()*100);
 }
@@ -64,38 +65,15 @@ function disableButton() {
   guessButton.disabled = (guessInputField.value === '');
 }
 
-
-
-
-
-resetButton.disabled = true;
-// var grabAndDisplayGuess = function(guess, guessInput,displayHint) {
-//   guess.addEventListener('click',function() {
-//     var guess = guessInput.value;
-//     checkValue(guess,displayHint);
-//     box.innerHTML = "<h1 id='latest-guess'>" + guess + "</h1>";
-//   });
-// };
-//
-// var checkValue = function (guess, displayHint) {
-//   if (guess===numberValue) {
-//     displayHint.innerHTML="You Won!! Nice Work!";
-//   } if (guess>numberValue) {
-//     displayHint.innerHTML="Your guess is too high! Try again.";
-//   } if (guess<numberValue) {
-//     displayHint.innerHTML="Your guess is too low! Please try again!";
-//   }
-// };
-
-
-
-// function clearInputField(selector, inputField) {
-//   selector.disabled = (inputField.value === '');
+// function startGame() {
+// resetButton.disabled = true;
 // }
-// clearInputField(guess, guessInput);
 
-//  “min” and a “max” attributes
-// display an error message if the guess is not a number (e.g. parseInt() returns NaN).
-// The application should display an error if the guess is outside of the range of possible answers.
-// The clear button should be disabled if there is nothing to clear.
-// The reset button should be disabled if there is nothing to reset.
+// startGame();
+// sets all the buttons to initial value
+// assigns a random value to randNum
+// calls disabledFunction()?
+
+// group your event listeners
+// in startGame: bindEventListeners();
+// bindEventListeners: gathers all event listeners in one function
